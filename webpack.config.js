bc81a85ab557c5221c69ef62d9f07c5aaaf2adab
aimergenge <onlylovesnow14@gmail.com>
2020-03-25 23:16:55 +0800

    modify filename scheme

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const dayjs = require('dayjs')
 
 module.exports = {
   mode: 'production',
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: '自动签到' + Date.now() + '.js'
+    filename: '自动签到' + getTime() + '.js'
   }
 };
+
+function getTime() {
+  return dayjs().format('YYYYMMDDHHmm');
+}
