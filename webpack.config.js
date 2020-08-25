0220d16437ac3e71e535884487c696d156f659cf
JiaCheng Wang <orange@JiaChengs-MacBook-Pro.local>
2020-08-25 14:59:13 +0800

    first release

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const path = require('path');
-const dayjs = require('dayjs')
+const dayjs = require('dayjs');
+const fs = require('fs')
 
-module.exports = {
-  mode: 'production',
-  entry: {
-    main: './src/main.js',
-    test: './src/test.js'
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: (chunkData) => {
-      return chunkData.chunk.name === 'main' ? ('自动签到' + getTime() + '.js') : 'test.js';
+module.exports = env => {
+  const entry = {
+    main: './src/main.js'
+  }
+  if (fs.existsSync(path.resolve(__dirname, './src/test.js'))) {
+    entry.test = './src/test.js'
+  }
+  return {
+    mode: 'production',
+    entry,
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: (chunkData) => {
+        return chunkData.chunk.name === 'main' ? ('auto-sign-' + getTime() + '.js') : 'test.js';
+      }
     }
   }
 };
