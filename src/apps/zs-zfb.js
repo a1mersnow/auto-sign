29dbf92924c64900c7412895da82fb3b9e950087
JiaCheng Wang <orange@JiaChengs-MacBook-Pro.local>
2020-08-26 11:05:21 +0800

    支付宝朋友全新改版

--- src/apps/zs-zfb.js
+++ src/apps/zs-zfb.js
@@ -5,62 +5,34 @@ let app = createApp('招行支付宝', 'com.eg.android.AlipayGphone', 'com.eg.an
   const el = text('稍后再说').findOne(MAX)
   if (el != null) clickControl(el)
 });
-app.add('点击朋友', (next) => {
-  findAndClickIt(text('朋友'));
-  next();
-}).add('看看有没有招商银行信用卡', (next) => {
-  const el = text('招商银行信用卡').findOne(MAX)
-  if (el) {
-    clickControl(el)
-    next('点击右下角')
-  } else {
-    next()
-  }
-}).add('点击生活号', (next) => {
-  findAndClickIt(text('生活号'));
-  next();
-}).add('先尝试搜索不行再点击已关注', (next) => {
-  const el = text('搜索你感兴趣的生活号').findOne(MAX)
-  if (el) {
-    clickControl(el)
-    next('输入招商银行信用卡')
-  } else {
-    findAndClickIt(text('已关注'));
-    next();
-  }
-}).add('点击搜索', (next) => {
-  let el
-  let el2
-  el = desc('搜索生活号').findOne(MAX)
-  if (el) {
-    clickControl(el)
-    next()
-  } else {
-    el2 = text('招商银行信用卡').findOne(MAX)
-    if (el2) {
-      scrollD(500)
-      sleep(100)
-      scrollD(500)
-      sleep(100)
-      scrollD(500)
-      sleep(100)
-      scrollD(500)
-      clickControl(el2)
-      next('点击右下角')
-    } else {
-      throw new Error('请先关注招商银行信用卡')
-    }
-  }
+app.add('点击搜索', (next) => {
+  findAndClickIt(idEndsWith('search_bg'))
+  next()
 }).add('输入招商银行信用卡', (next) => {
-  let el = text('搜索生活号').className('android.widget.EditText').findOne(MAX);
-  if (el == null) throw new Error();
-  el.setText('招商银行信用');
+  let el = idEndsWith('search_input_box').className('android.widget.EditText').findOne(MAX);
+  if (el == null) throw new Error('没有找到输入框');
+  el.setText('招商银行信用卡');
   next();
 }).add('点击搜索结果中的招行信用卡', (next) => {
-  let el = text('已关注').findOne(MAX);
-  if (el == null) throw new Error('请先关注招商银行信用卡生活号');
-  clickControl(el);
-  next();
+  findAndClickIt(text('生活号'))
+  next()
+}).add('选择招商银行信用卡', (next) => {
+  findAndClickIt(text('招商银行信用卡'))
+  next()
+}).add('同意并继续', (next) => {
+  const el = text('同意并继续').findOne(1000)
+  if (el) {
+    clickControl(el)
+    next()
+  } else {
+    next('点击右下角')
+  }
+}).add('关注', (next) => {
+  findAndClickIt(text('关注生活号'))
+  next()
+}).add('拒绝定位', (next) => {
+  findAndClickIt(text('拒绝'))
+  next()
 }).add('点击右下角', (next) => {
   click(device.width - 70, device.height - 70);
   next();
@@ -79,15 +51,5 @@ app.add('点击朋友', (next) => {
   findAndClickIt(text('签到按钮图片'));
   next();
 });
-
-/**
- * @param {number} dis
- */
-function scrollD (dis) {
-  const x = device.width / 2
-  const y = device.height / 2
-  swipe(x, y, x, y + dis, 100)
-}
-
 
 export default app;
