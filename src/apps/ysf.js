3da0f33233d1d249258a5354ff19528ce5ff9007
orange <onlylovesnow14@gmail.com>
2021-06-04 20:02:15 +0800

    fix ysf

--- src/apps/ysf.js
+++ src/apps/ysf.js
@@ -2,7 +2,7 @@ import {findAndClickIt, clickControl, backward, getNumberFromSelector, MAX, sibl
 import {createApp} from '../app';
 
 let app = createApp('云闪付', 'com.unionpay', () => {
-  return text('首 页').exists() && text('我 的').exists();
+  return text('首页').exists() && text('我的').exists();
 }, undefined, false, () => {
   let el = text('稍候再说').findOne(MAX)
   if (el) {
@@ -10,10 +10,15 @@ let app = createApp('云闪付', 'com.unionpay', () => {
   }
 });
 app.add('点击我的', (next) => {
-  findAndClickIt(text('我 的'));
+  findAndClickIt(text('我的'));
   next();
 }).add('点击签到入口', next => {
-  findAndClickIt(idEndsWith('iv_sign_in'))
+  let el = idEndsWith('iv_sign_in_down').findOne(MAX)
+  if (el == null) {
+    el = idEndsWith('iv_sign_in').findOnce()
+  }
+  if (!el) throw new Error()
+  clickControl(el)
   sleep(2000)
   next()
 }).add('点击立即签到', next => {
