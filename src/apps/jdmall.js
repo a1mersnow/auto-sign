e3b8ecee2c4f9b4987a4e45c94d17597be1f6acb
a1mer <onlylovesnow14@gmail.com>
2021-08-27 10:19:50 +0800

    京东只保留签到和双签
    有需要的可以用 https://github.com/chinnkarahoi/jd_scripts

--- src/apps/jdmall.js
+++ src/apps/jdmall.js
@@ -30,118 +30,8 @@ app.add('点击我的', (next) => {
     }
   }
   next();
-}).add('抽京豆', (next) => {
-  while (true) {
-    let el = findEntry(-2)
-    if (el) {
-      clickControl(el, true)
-      let ref = text('我的奖品').findOne(MAX)
-      // @ts-ignore
-      let numEl = ref.parent().parent().parent().child(4)
-      if (numEl) {
-        let match = /今日还可以抽\s*([0-9]+)\s*次哦/.exec(numEl.text())
-        if (match && match[1] && +match[1] > 0) {
-          let el = text('我的奖品').findOne(MAX)
-          // @ts-ignore
-          let p = el.parent().parent().parent().child(3)
-          if (p) {
-            clickControl(p, true)
-            sleep(2000)
-            backward()
-          }
-        } else {
-          backward()
-          log('次数不够了')
-          break
-        }
-      } else {
-        backward()
-        log('没有获取到次数信息')
-        break
-      }
-    } else {
-      break
-    }
-  }
-  next()
-}).add('摇京豆', (next) => {
-  let el = findEntry(-1);
-  if (el) {
-    clickControl(el, true);
-    sleep(8000);
-
-    click(device.width / 2, device.height * 8 / 9);
-    closeDialog()
-
-    openTasks()
-
-    doTasks();
-
-    let t
-    while ((t = textMatches(/^.*摇一摇\s*?有惊喜.*$/).findOnce())) {
-      clickControl(t, true)
-      sleep(1000)
-      closeDialog()
-      sleep(300)
-    }
-
-    closePage()
-  }
-
-  function closeDialog () {
-    let close = text('Close').findOnce()
-    if (close) clickControl(close, true)
-    let more = textStartsWith('浏览其他').findOnce()
-    if (more) clickControl(more, true)
-  }
-
-  function doTasks () {
-    let el = text('浏览活动').findOnce()
-    if (el) {
-      scrollU(300, device.height * 3 / 4, 300)
-      sleep(300)
-      scrollU(300, device.height * 3 / 4, 300)
-      sleep(300)
-      scrollU(300, device.height * 3 / 4, 300)
-      sleep(300)
-      scrollU(300, device.height * 3 / 4, 300)
-      sleep(300)
-      scrollU(300, device.height * 3 / 4, 300)
-      let t
-      while ((t = text('浏览').findOnce())) {
-        t.click()
-        sleep(2000)
-        backward()
-        sleep(1000)
-      }
-      let c = text('Close').findOnce()
-      if (c) clickControl(c, true)
-    }
-  }
-
-  function openTasks () {
-    // @ts-ignore
-    let root = text('天天买爆品').findOne(MAX).parent()
-    if (root) {
-      let z = root.child(1)
-      if (z) {
-        clickControl(z, true)
-        sleep(1000)
-      }
-    }
-  }
-
-  function closePage () {
-    let el = desc('关闭页面').findOnce()
-    if (el == null) el = desc('返回').findOnce()
-    if (el) clickControl(el)
-  }
-
-  next();
 }).add('点击双签', (next) => {
-  let el = findEntry(-5)
-  if (!el)  throw new Error()
-  clickControl(el, true)
+  click(50, device.height * 0.6);
   next();
 }).add('点击立即领取', (next) => {
   let el = text('立即领取').findOne(MAX);
@@ -150,9 +40,7 @@ app.add('点击我的', (next) => {
     next('查看京豆数量');
   } else {
     backward();
-    let el = findEntry(-5)
-    if (!el)  throw new Error()
-    clickControl(el, true)
+    click(50, device.height * 0.6);
     next();
   }
 }).add('点击`完成双签领取`', (next) => {
@@ -173,9 +61,7 @@ app.add('点击我的', (next) => {
   }
   combo();
   backward();
-  let el = findEntry(-5) || text('双签领豆').findOne(MAX)
-  if (!el)  throw new Error()
-  clickControl(el, true)
+  click(50, device.height * 0.6)
   combo();
   next();
 }).add('查看京豆数量', (next, tools) => {
@@ -189,19 +75,5 @@ app.add('点击我的', (next) => {
   next();
 });
 
-/**
- *
- * @param {number} childIndex
- */
-function findEntry (childIndex) {
-  let el = textMatches(/^(签到领京豆|.*已连续签到.*|.*已连签.*)$/).findOne(MAX);
-  if (el == null) throw new Error('“签到领京豆”或“已连续签到”按钮不存在，无法继续找到入口');
-  // @ts-ignore
-  let c = el.parent().parent().parent().parent();
-  if (!c) throw new Error('未找到容器');
-  let len = c.childCount();
-  let i = len + childIndex;
-  return c.child(i);
-}
 
 export default app;
